396fc9550e0de190c4e9f814cc562db43cb7708c
galroze <gal.rozenfeld87@gmail.com>
2018-12-29 18:50:59 +0200

    Generate original system description metrics and write to CSV

--- Experiments/ExperimentServiceImpl.py
+++ Experiments/ExperimentServiceImpl.py
@@ -2,6 +2,7 @@ from Experiments.ConfigurationsDAO import *
 from Experiments.IterationsDAO import *
 from Experiments.ExperimentsDAO import *
 from LogicTypes import *
+from LogicUtils import *
 
 
 def write_experiment(git_version, run_time, ALCS_configuration, metrics_by_iteration):
@@ -43,10 +44,6 @@ def write_experiment(git_version, run_time, ALCS_configuration, metrics_by_itera
 
 def convert_boolean(value):
     return 1 if value else 0
-
-
-def get_metric_to_persist(metric, entry):
-    return metric[entry] if metric.get(entry) is not None else -1
 
 
 def escape_str(value):
